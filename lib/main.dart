ab64aac437baa15ce49f179830e4258256352c6c
Alexander P <panshin.alexander@yahoo.com>
2021-07-18 20:59:31 +0300

    страничка пользователя

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,47 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(MaterialApp(
+  home: UserPanel(),
+));
 
-class MyApp extends StatelessWidget {
+class UserPanel extends StatelessWidget {
+  const UserPanel({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(primaryColor: Colors.grey),
-
-      home: Scaffold(
-
-        appBar: AppBar(
-          title: Text('My App'),
-          centerTitle: true,
-        ),
-
-        body: Container(
-          color: Colors.red,
-          margin: EdgeInsets.all(45.0),
-          padding: EdgeInsets.all(20.0),
-          child: Text('text'),
+    return Scaffold(
+      backgroundColor: Colors.blueGrey,
+      appBar: AppBar(
+        title: Text('User panel'),
+        centerTitle: true,
+        backgroundColor: Colors.lime,
       ),
-
-    ));
+      body: SafeArea(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Column(
+              children: [
+                Padding(padding: EdgeInsets.only(top: 20.0)),
+                Text('mr. John', style: TextStyle(fontSize: 25.0)),
+                Padding(padding: EdgeInsets.only(top: 15.0)),
+                CircleAvatar(
+                  backgroundImage: AssetImage('images/getty-googleplex-office.jpg'),
+                  radius: 40.0,
+                ),
+                Padding(padding: EdgeInsets.only(top: 30.0)),
+                Row(
+                  children: [
+                    Icon(Icons.email, color: Colors.blue,),
+                    Padding(padding: EdgeInsets.symmetric(horizontal: 10.0)),
+                    Text('john@google.com', style: TextStyle(color: Colors.blue),)
+                  ],
+                )
+              ],
+            )
+          ],
+        )
+      ),
+    );
   }
-
 }
