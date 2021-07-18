31b99a77b8c9de9426596cfc4545af1c4c264ac2
Alexander P <panshin.alexander@yahoo.com>
2021-07-18 18:24:47 +0300

    кнопка, стили, шрифт

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,37 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-  home: Text('my text'),
-));
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(primaryColor: Colors.grey),
+
+      home: Scaffold(
+
+        appBar: AppBar(
+          title: Text('My App'),
+          centerTitle: true,
+        ),
+
+        body: Center(
+          child: Text('My text', style: TextStyle(
+              fontSize: 20,
+              color: Colors.deepPurple,
+              fontFamily: 'Times New Roman'
+          ),),
+        ),
+
+        floatingActionButton: FloatingActionButton(
+          onPressed: () { print('CLICK!'); },
+          child: Text('BUTTON'),
+          backgroundColor: Colors.green,
+        ),
+
+      ),
+
+    );
+  }
+
+}
