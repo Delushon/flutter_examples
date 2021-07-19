0dfe52243d018a624d4b49aa9b9b392b8bdbaeeb
Alexander P <panshin.alexander@yahoo.com>
2021-07-19 23:49:23 +0300

    Работа с БД Firebase, добавление и удаление элементов списка, загрузка

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_apps/pages/home.dart';
 import 'package:flutter_apps/pages/main_screen.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() => runApp(MaterialApp(
-  theme: ThemeData(
-    primaryColor: Colors.lightGreen
-  ),
-  initialRoute: '/',
-  routes: {
-    '/': (context) => MainScreen(),
-    '/todo': (context) => Home()
-  },
-));
+void initFirebase() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+}
+
+void main() {
+  initFirebase();
+  runApp(MaterialApp(
+    theme: ThemeData(
+        primaryColor: Colors.lightGreen
+    ),
+    initialRoute: '/',
+    routes: {
+      '/': (context) => MainScreen(),
+      '/todo': (context) => Home()
+    },
+  ));
+}
